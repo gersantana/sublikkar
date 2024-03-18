1135e02cb240d6efcdd4f4d15b7ccce9549f74d9
m4xcav <cavero.maximo.l@gmail.com>
2024-03-18 17:01:08 -0300

    :ok_hand: adecuando el navbar para que se vea como en la maqueta

--- src/components/buscador/Buscador.jsx
+++ src/components/buscador/Buscador.jsx
@@ -2,7 +2,7 @@
 export const Buscador = () => {
   return (
     <div className="mb-3 xl:w-96">
-          <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="relative flex w-full  items-stretch">
               <input
                   type="search"
                   className="relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
@@ -12,7 +12,8 @@ export const Buscador = () => {
 
               {/* <!--Search icon--> */}
               <span
-                  className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
+                  className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200
+                  rounded-r-lg bg-[#4fb4b9]"
                   id="basic-addon2">
                   <svg
                       xmlns="http://www.w3.org/2000/svg"
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,74 +1,90 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-scroll";
 import {Buscador} from "../buscador/Buscador";
 import {useState} from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaTimes } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
 
 export const NavBar = () => {
 	const [click, setClick] = useState(false);
 	const darClick = () => setClick(!click);
 	const contenido = <>
-	<div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
-		{/* BUSCADOR E INICIO SESION */}
-
-		{/* BUSCADOR E INICIO SESION */}
-		<ul className="text-center text-xl p-20">
-		<NavLink spy={true} smoorth={true} to="/">
+	<div className="lg:hidden block absolute top-[14%] w-full left-0 right-0 bg-slate-400 transition">
+		<ul className="text-center text-xl p-10">
+		<Link spy={true} smooth={true} to="/">
 			<li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Inicio</li>
-		</NavLink>
-		<NavLink spy={true} smoorth={true} to="/">
+		</Link>
+		<Link spy={true} smooth={true} to="/">
 			<li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Comprar Todo</li>
-		</NavLink>
-		<NavLink spy={true} smoorth={true} to="/">
+		</Link>
+		<Link spy={true} smooth={true} to="/">
 			<li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Acerca de</li>
-		</NavLink>
-		<NavLink spy={true} smoorth={true} to="/">
+		</Link>
+		<Link spy={true} smooth={true} to="/">
 			<li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Blog</li>
-		</NavLink>
-		<NavLink spy={true} smoorth={true} to="/">
+		</Link>
+		<Link spy={true} smooth={true} to="/">
 			<li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Contacto</li>
-		</NavLink>
-		<NavLink spy={true} smoorth={true} to="/">
+		</Link>
+		<Link spy={true} smooth={true} to="/">
 			<li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Miembros</li>
-		</NavLink>
+		</Link>
 		</ul>
 	</div>
 	</>
   return (
 	<nav>
-		<div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
-			<div className="flex items-center flex-1">
-				<img src="./img/logo.png" alt="logo" />
+		<div className="flex items-center justify-between gap-2">
+				<div className="ml-10 mt-5">
+					<img src="./img/logo.png" alt="logo" />
+				</div>
+				<div className="mt-5">
+				<Buscador></Buscador>
+				</div>
+				<div className="flex items-center justify-start mr-10">
+				<ul className="flex gap-5 mr-10 text-[14px] text-white">
+					<Link spy={true} smooth={true} to="/">
+						<li className="p-4 bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Ingresar</li>
+					</Link>
+					<Link spy={true} smooth={true} to="/">
+						<li className="p-4 bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Registrarce</li>
+					</Link>
+					<Link><li className="p-5 bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+						<FaCartShopping></FaCartShopping>
+						</li></Link>
+					</ul>
+				</div>
+				
 			</div>
-			<Buscador></Buscador>
-			<div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
+		<div className="h-10vh flex justify-between z-10 text-white lg:py-5 px-20 py-1">
+			<div className="lg:flex md:flex lg: flex-1 items-center justify-center font-normal hidden">
 				<div className= "flex-10">
-					<ul className="flex gap-8 mr-16 text-[18px]">
-					<NavLink spy={true} smoorth={true} to="/">
-						<li className="bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Inicio</li>
-					</NavLink>
-					<NavLink spy={true} smoorth={true} to="/">
-						<li className="bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Comprar Todo</li>
-					</NavLink>
-					<NavLink spy={true} smoorth={true} to="/">
-						<li className="bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Acerca de</li>
-					</NavLink>
-					<NavLink spy={true} smoorth={true} to="/">
-						<li className="bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Blog</li>
-					</NavLink>
-					<NavLink spy={true} smoorth={true} to="/">
-						<li className="bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Contacto</li>
-					</NavLink>
-					<NavLink spy={true} smoorth={true} to="/">
-						<li className="bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Miembros</li>
-					</NavLink>
+					<ul className="flex gap-5 mr-10 text-[14px]">
+					<Link spy={true} smooth={true} to="/">
+						<li className="p-4 bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Inicio</li>
+					</Link>
+					<Link spy={true} smooth={true} to="/">
+						<li className="p-4 bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Comprar Todo</li>
+					</Link>
+					<Link spy={true} smooth={true} to="/">
+						<li className="p-4 bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Acerca de</li>
+					</Link>
+					<Link spy={true} smooth={true} to="/">
+						<li className="p-4 bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Blog</li>
+					</Link>
+					<Link spy={true} smooth={true} to="/">
+						<li className="p-4 bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Contacto</li>
+					</Link>
+					<Link spy={true} smooth={true} to="/">
+						<li className="p-4 bg-[#4fb4b9] rounded-full hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Miembros</li>
+					</Link>
 					</ul>
 				</div>
 			</div>
 			<div>
 				{click && contenido};
 			</div>
-			<button className="block sm:hidden transtion" onclick={darClick}>
+			<button className="block md:hidden transtion text-black" onClick={darClick}>
 				{click ? <FaTimes/> : <GiHamburgerMenu/> } 
 			</button>
 		</div>
